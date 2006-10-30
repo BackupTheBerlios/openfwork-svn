35abe83bd5ee5eb5d5a04aed63e343cffaa8ad34
via11 <via11@7b2a86ad-2c20-0410-b595-cb3d780405bd>
2006-10-30 10:28:11 +0000

    закомитил не по децки

---
+++ trunk/client.php
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="windows-1251"?> 
+<html>
+	<table>
+	<tr>
+		<td>
+			<FORM action="client.php">
+			<BUTTON type="submit" name="table" value="indoc">
+				Журнал входящих документов от юридических лиц
+			</BUTTON>
+			<br>
+			<BUTTON type="submit" name="table" value="ul">
+				Журнал юридических лиц
+			</BUTTON>
+			</FORM>
+		</td>
+		<td>
+		</td>
+	</tr>
+	</table>
+</html>
